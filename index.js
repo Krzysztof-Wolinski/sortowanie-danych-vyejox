ce9c7662450d611e9db1bc1c3dcc98aca8487b02
Wolin <wolin51@gmail.com>
2021-08-29 19:22:00 +0200

    update

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Tutaj napisz swój kod
+
+const trElements = document.querySelectorAll('tbody tr');
+const buttonSort = document.querySelector('a');
+
+
+const newData = new Object(
+    // {module: "", autor: "", time: ""},
+)
+
+console.log(newData)
+const sortData = () => {
+    console.log('kliknąłeś mnie');
+    trElements.forEach((element, index) => {
+        const tdElement = element.querySelectorAll('td');
+        console.log(tdElement)
+        tdElement.forEach(td => {
+            console.log(td)
+        })
+    });
+    console.log(newData)
+}
+
+
+
+buttonSort.addEventListener('click', sortData);
